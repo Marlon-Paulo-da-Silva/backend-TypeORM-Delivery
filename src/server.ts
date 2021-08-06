adbc0fd3b619150f00c537c010eb529de4631b90
unknown <marlon.paulo.silva@outlook.com>
2021-08-06 14:10:06 -0300

    atualização no server para deploy

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-require('dotenv').config({path: 'file.env'});
-
+const dotenv = require('dotenv');
 import app from './app';
 import 'reflect-metadata';
 import './database';
 
+dotenv.config();
 
 app.listen(3333, ()=>{
     console.log('Running Server');
