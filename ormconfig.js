8a7cc539a4762d897813843a4914d3036fafbb14
unknown <marlon.paulo.silva@outlook.com>
2021-08-06 15:06:05 -0300

    atualização no server para deploy no heroku6

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,10 @@
 module.exports = {
    "type": "postgres",
    "url": process.env.DATABASE_URL,
+   "ssl": true,
+   "extra": {
+       "ssl": { "rejectUnauthorized": false }
+      },
    "entities": [
       "build/models/**/*.js"
    ],
